6f55823401b489c3dfaa7c70b1df488c127c5b2d
Summer Banister <sgbanister@gmail.com>
2019-10-14 10:35:23 -0700

    Edited milestone_1

--- Milestone_1.cpp
+++ Milestone_1.cpp
@@ -43,6 +43,7 @@ void string_to_int(string const &the_string, vector<int> &int_vector)
     }
     // I think something is going wrong in converting the string to numbers, in the beginning. 
     // Maybe try using stoi instead and see if that makes a difference?
+    // Nope. Did not make a difference.
 }
 
 
